001e2446b823371495e8a6a475e3422846dc1ba5
Lucas Gutierres <luquitas1997.7@hotmail.com>
2021-08-21 22:24:30 -0300

    Added Diagram, modified product.js

--- js/prueba.js
+++ js/prueba.js
@@ -1,38 +1,44 @@
-document.addEventListener("DOMContentLoaded", function(e){
-            fetch(PRODUCTS_URL)
-             .then(result => result.json())
-             .then(data => {
-                for(let i = 0; i < data.length; i++){
-                    let auto = data[i];                
-                let description = auto.description;
-                let cost = auto.cost;
-                let currency = auto.currency;
-                let name = auto.name;
-                let image = auto.imgSrc;
-                console.log(data);
+// var productArray = [];
 
-                let product = "";
+// function showProductList(array){
 
-                product += `
-                <div class="list-group-item list-group-item-action">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="` + image + `" alt="` + description + `" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ name +`</h4>
-                            <small class="text-muted">` + currency + ' ' + cost + `</small>
-                        </div>
-                    
-                        <div class="text-muted"> <h5>` + description + `</h5></div>
-                    </div>
-                    
-                </div>
-            </div>
-            `
-
-            document.getElementById("cat-list-container").innerHTML += product;
-                };
-            });
-             });
+//     let htmlContentToAppend = "";
+//     for(let i = 0; i < array.length; i++){
+//         let product = array[i];
+//         htmlContentToAppend += `
+//         <div class="list-group-item list-group-item-action">
+//             <div class="row">
+//                 <div class="col-3">
+//                     <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+//                 </div>
+//                 <div class="col">
+//                     <div class="d-flex w-100 justify-content-between">
+//                         <h4 class="mb-1">`+ product.name +`</h4>
+//                         <small class="text-muted">` + product.currency + ' ' + product.cost + `</small>
+//                     </div>
+                
+//                     <div class="text-muted"> <h5>` + product.description + `</h5></div>
+//                 </div>
+                
+//             </div>
+//         </div>
+//         `
+//         // En caso de fallar la imagen se muestra la descripción, por eso es el alt de la linea 12 
+        
+//         document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+//     }
+// }
+// //Función que se ejecuta una vez que se haya lanzado el evento de
+// //que el documento se encuentra cargado, es decir, se encuentran todos los
+// //elementos HTML presentes.
+// document.addEventListener("DOMContentLoaded", function(){
+//     getJSONData(PRODUCTS_URL).then(function(resultObj){
+//         if (resultObj.status === "ok")
+//         {
+//             productArray = resultObj.data;
+//             //Muestro las categorías ordenadas
+//             showProductList(productArray);
+//         }
+//     });
+    
+// });
