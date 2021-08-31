8ee9265836452142a81080b18d1ce93a1222c3ed
Lucas Gutierres <luquitas1997.7@hotmail.com>
2021-08-31 00:33:43 -0300

    added sort dec and sort relev

--- js/products.js
+++ js/products.js
@@ -1,159 +1,248 @@
 document.addEventListener("DOMContentLoaded", function (e) {
-    var maximo = null;
-    var minimo = null;
+  var maximo = null;
+  var minimo = null;
+  
+  function showCars() {
+    fetch(PRODUCTS_URL)
+      .then((result) => result.json())
+      .then((data) => {
+        for (let i = 0; i < data.length; i++) {
+          let auto = data[i];
+          let description = auto.description;
+          let cost = auto.cost;
+          let currency = auto.currency;
+          let name = auto.name;
+          let image = auto.imgSrc;
+          let sold = auto.soldCount;
+          let product = "";
+          product +=
+          `
+          <div class="list-group-item list-group-item-action">
+          <div class="row">
+              <div class="col-3">
+                  <img src="` + image +`" alt="` + description + `" class="img-thumbnail">
+              </div>
+              <div class="col">
+                  <div class="d-flex w-100 justify-content-between">
+                      <h4 class="mb-1">` + name + `</h4>
+                      <small class="text-muted">` + currency + " " + cost + `</small>
+                  </div>
+                  <div class="d-flex w-100 justify-content-between">
+                  <h4 class="mb-1"> </h4>
+                      <small class="text-muted"> <b> Vendidos:` + " " + sold + `</b></small>
+                  </div>
+                  <div class="text-muted"> <h5>` + description + `</h5></div>
+              </div>
+          </div>
+      </div>
+      `;
+          document.getElementById("cat-list-container").innerHTML += product;
+        }
+      });
+  }
 
-        function showCars() {
-        fetch(PRODUCTS_URL)
-            .then((result) => result.json())
-            .then((data) => {
-                for (let i = 0; i < data.length; i++) {
-                    let auto = data[i];
-                    let description = auto.description;
-                    let cost = auto.cost;
-                    let currency = auto.currency;
-                    let name = auto.name;
-                    let image = auto.imgSrc;
-                    let sold = auto.soldCount;
-                    let product = "";
-                    product +=
-                        `
-                    <div class="list-group-item list-group-item-action">
-                    <div class="row">
-                        <div class="col-3">
-                            <img src="` + image +`" alt="` + description + `" class="img-thumbnail">
-                        </div>
-                        <div class="col">
-                            <div class="d-flex w-100 justify-content-between">
-                                <h4 class="mb-1">` + name + `</h4>
-                                <small class="text-muted">` + currency + " " + cost + `</small>
-                            </div>
-                            <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1"> </h4>
-                                <small class="text-muted"> <b> Vendidos:` + " " + sold + `</b></small>
-                            </div>
-                            <div class="text-muted"> <h5>` + description + `</h5></div>
-                        </div>
-                    </div>
+  function showFilterCars() {
+    let productFiltrado = "";
+    fetch(PRODUCTS_URL)
+      .then((result) => result.json())
+      .then((data) => {
+        for (let i = 0; i < data.length; i++) {
+          let auto = data[i];
+          let description = auto.description;
+          let cost = auto.cost;
+          let currency = auto.currency;
+          let name = auto.name;
+          let image = auto.imgSrc;
+          let sold = auto.soldCount;
+          if (
+            (minimo == null || (minimo != null && cost >= minimo)) &&
+            (maximo == null || (maximo != null && cost <= maximo))
+          ) {
+            productFiltrado +=
+            `
+            <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + image +`" alt="` + description + `" class="img-thumbnail">
                 </div>
-                `;
-                    document.getElementById("cat-list-container").innerHTML += product;
-                }
-            });
-    }
-
-    function showFilterCars() {
-        let productFiltrado = "";
-        fetch(PRODUCTS_URL)
-            .then((result) => result.json())
-            .then((data) => {
-                for (let i = 0; i < data.length; i++) {
-                    let auto = data[i];
-                    let description = auto.description;
-                    let cost = auto.cost;
-                    let currency = auto.currency;
-                    let name = auto.name;
-                    let image = auto.imgSrc;
-                    let sold = auto.soldCount;
-                    if (
-                        (minimo == null || (minimo != null && cost >= minimo)) &&
-                        (maximo == null || (maximo != null && cost <= maximo))
-                    ) {
-                        productFiltrado +=
-                        `
-                        <div class="list-group-item list-group-item-action">
-                        <div class="row">
-                            <div class="col-3">
-                                <img src="` + image +`" alt="` + description + `" class="img-thumbnail">
-                            </div>
-                            <div class="col">
-                                <div class="d-flex w-100 justify-content-between">
-                                    <h4 class="mb-1">` + name + `</h4>
-                                    <small class="text-muted">` + currency + " " + cost + `</small>
-                                </div>
-                                <div class="d-flex w-100 justify-content-between">
-                                <h4 class="mb-1"> </h4>
-                                    <small class="text-muted"> <b> Vendidos:` + " " + sold + `</b></small>
-                                </div>
-                                <div class="text-muted"> <h5>` + description + `</h5></div>
-                            </div>
-                        </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">` + name + `</h4>
+                        <small class="text-muted">` + currency + " " + cost + `</small>
                     </div>
-                    `;
-                        document.getElementById("cat-list-container").innerHTML =
-                            productFiltrado;
-                    }
-                }
-            });
-    }
+                    <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1"> </h4>
+                        <small class="text-muted"> <b> Vendidos:` + " " + sold + `</b></small>
+                    </div>
+                    <div class="text-muted"> <h5>` + description + `</h5></div>
+                </div>
+            </div>
+        </div>
+        `;
+            document.getElementById("cat-list-container").innerHTML =
+              productFiltrado;
+          }
+        }
+      });
+  }
 
-
-function sortAscendiente() {
+  function sortAscendiente() {
     let productAscendiente = "";
-        fetch(PRODUCTS_URL)
-            .then((result) => result.json())
-            .then((data) => {
-                console.log(data)
-                data.sort(function(a, b) {
-                         return (a.cost) - (b.cost);
-                      });
-                console.log(data)
-                for (let i = 0; i < data.length; i++) {
-                    let auto = data[i];
-                    let description = auto.description;
-                    let cost = auto.cost;
-                    let currency = auto.currency;
-                    let name = auto.name;
-                    let image = auto.imgSrc;
-                    let sold = auto.soldCount;
-                        productAscendiente +=
-                        `
-                        <div class="list-group-item list-group-item-action">
-                        <div class="row">
-                            <div class="col-3">
-                                <img src="` + image +`" alt="` + description + `" class="img-thumbnail">
-                            </div>
-                            <div class="col">
-                                <div class="d-flex w-100 justify-content-between">
-                                    <h4 class="mb-1">` + name + `</h4>
-                                    <small class="text-muted">` + currency + " " + cost + `</small>
-                                </div>
-                                <div class="d-flex w-100 justify-content-between">
-                                <h4 class="mb-1"> </h4>
-                                    <small class="text-muted"> <b> Vendidos:` + " " + sold + `</b></small>
-                                </div>
-                                <div class="text-muted"> <h5>` + description + `</h5></div>
-                            </div>
-                        </div>
-                    </div>
-                    `;
-                        document.getElementById("cat-list-container").innerHTML =
-                            productAscendiente;
-                    }
-                    })
-            };
+    fetch(PRODUCTS_URL)
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data);
+        data.sort(function (a, b) {
+          return a.cost - b.cost;
+        });
+        console.log(data);
+        for (let i = 0; i < data.length; i++) {
+          let auto = data[i];
+          let description = auto.description;
+          let cost = auto.cost;
+          let currency = auto.currency;
+          let name = auto.name;
+          let image = auto.imgSrc;
+          let sold = auto.soldCount;
+          productAscendiente +=
+          `
+          <div class="list-group-item list-group-item-action">
+          <div class="row">
+              <div class="col-3">
+                  <img src="` + image +`" alt="` + description + `" class="img-thumbnail">
+              </div>
+              <div class="col">
+                  <div class="d-flex w-100 justify-content-between">
+                      <h4 class="mb-1">` + name + `</h4>
+                      <small class="text-muted">` + currency + " " + cost + `</small>
+                  </div>
+                  <div class="d-flex w-100 justify-content-between">
+                  <h4 class="mb-1"> </h4>
+                      <small class="text-muted"> <b> Vendidos:` + " " + sold + `</b></small>
+                  </div>
+                  <div class="text-muted"> <h5>` + description + `</h5></div>
+              </div>
+          </div>
+      </div>
+      `;
+          document.getElementById("cat-list-container").innerHTML =
+            productAscendiente;
+        }
+      });
+  }
 
+  function sortDescendiente() {
+    let productDescendiente = "";
+    fetch(PRODUCTS_URL)
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data);
+        data.sort(function (a, b) {
+          return b.cost - a.cost;
+        });
+        console.log(data);
+        for (let i = 0; i < data.length; i++) {
+          let auto = data[i];
+          let description = auto.description;
+          let cost = auto.cost;
+          let currency = auto.currency;
+          let name = auto.name;
+          let image = auto.imgSrc;
+          let sold = auto.soldCount;
+          productDescendiente +=
+          `
+          <div class="list-group-item list-group-item-action">
+          <div class="row">
+              <div class="col-3">
+                  <img src="` + image +`" alt="` + description + `" class="img-thumbnail">
+              </div>
+              <div class="col">
+                  <div class="d-flex w-100 justify-content-between">
+                      <h4 class="mb-1">` + name + `</h4>
+                      <small class="text-muted">` + currency + " " + cost + `</small>
+                  </div>
+                  <div class="d-flex w-100 justify-content-between">
+                  <h4 class="mb-1"> </h4>
+                      <small class="text-muted"> <b> Vendidos:` + " " + sold + `</b></small>
+                  </div>
+                  <div class="text-muted"> <h5>` + description + `</h5></div>
+              </div>
+          </div>
+      </div>
+      `;
+          document.getElementById("cat-list-container").innerHTML =
+            productDescendiente;
+        }
+      });
+  }
 
+  function sortRelev() {
+    let productRelev = "";
+    fetch(PRODUCTS_URL)
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data);
+        data.sort(function (a, b) {
+          return b.soldCount - a.soldCount;
+        });
+        console.log(data);
+        for (let i = 0; i < data.length; i++) {
+          let auto = data[i];
+          let description = auto.description;
+          let cost = auto.cost;
+          let currency = auto.currency;
+          let name = auto.name;
+          let image = auto.imgSrc;
+          let sold = auto.soldCount;
+          productRelev +=
+          `
+          <div class="list-group-item list-group-item-action">
+          <div class="row">
+              <div class="col-3">
+                  <img src="` + image +`" alt="` + description + `" class="img-thumbnail">
+              </div>
+              <div class="col">
+                  <div class="d-flex w-100 justify-content-between">
+                      <h4 class="mb-1">` + name + `</h4>
+                      <small class="text-muted">` + currency + " " + cost + `</small>
+                  </div>
+                  <div class="d-flex w-100 justify-content-between">
+                  <h4 class="mb-1"> </h4>
+                      <small class="text-muted"> <b> Vendidos:` + " " + sold + `</b></small>
+                  </div>
+                  <div class="text-muted"> <h5>` + description + `</h5></div>
+              </div>
+          </div>
+      </div>
+      `;
+          document.getElementById("cat-list-container").innerHTML =
+            productRelev;
+        }
+      });
+  }
 
+  showCars();
+  document.getElementById("limpiar").onclick = function () {
+    document.getElementById("filterMin").value = "";
+    document.getElementById("filterMax").value = "";
+    document.getElementById("cat-list-container").innerHTML = "";
     showCars();
-    document.getElementById("limpiar").onclick = function () {
-        document.getElementById("filterMin").value = "";
-        document.getElementById("filterMax").value = "";
-        document.getElementById("cat-list-container").innerHTML = "";
-        showCars();
-    };
-    document.getElementById("filtro").onclick = function () {
-        minimo = document.getElementById("filterMin").value;
-        maximo = document.getElementById("filterMax").value;
-        showFilterCars();
-    };
+  };
+  document.getElementById("filtro").onclick = function () {
+    minimo = document.getElementById("filterMin").value;
+    maximo = document.getElementById("filterMax").value;
+    showFilterCars();
+  };
 
-    document.getElementById("ascendiente").onclick = function () {
-        sortAscendiente()
-    };
+  document.getElementById("ascendiente").onclick = function () {
+    sortAscendiente();
+  };
 
-    document.getElementById("descendiente").onclick = function () {
-    };
+  document.getElementById("descendiente").onclick = function () {
+    sortDescendiente();
+  };
 
-    document.getElementById("cantidad").onclick = function () {
-    };
+  document.getElementById("cantidad").onclick = function () {
+    sortRelev();
+  };
 });
