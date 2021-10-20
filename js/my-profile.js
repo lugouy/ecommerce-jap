ffba53f3a31072759bc03b77c0471ba89fd0c006
Lucas Gutierres <luquitas1997.7@hotmail.com>
2021-10-20 02:39:41 -0300

    modified my profile function

--- js/my-profile.js
+++ js/my-profile.js
@@ -2,53 +2,35 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    function setear() {
+        let datos = {firstName: document.getElementById('firstName').value, secondName: document.getElementById('secondName').value, firstLastName: document.getElementById('firstLastName').value, secondLastName: document.getElementById('secondLastName').value, email: document.getElementById('email').value, phone: document.getElementById('phone').value}
+        localStorage.setItem('datosJSON', JSON.stringify(datos));
+        swal("¡Sus cambios se han guardado con éxito! :)", {
+            icon: "success",
+            }
+          )
+          .then(function() {
+            window.location.href = "my-profile.html";
+          });    
+    }
     
 document.getElementById("guardar").onclick = function(e) { 
     if (document.getElementById('firstName').value && document.getElementById('firstLastName').value && document.getElementById('email').value && document.getElementById('phone').value) {
-    localStorage.setItem('firstName', document.getElementById('firstName').value);
-    localStorage.setItem('secondName', document.getElementById('secondName').value);
-    localStorage.setItem('firstLastName', document.getElementById('firstLastName').value);
-    localStorage.setItem('secondLastName', document.getElementById('secondLastName').value);
-    localStorage.setItem('email', document.getElementById('email').value);
-    localStorage.setItem('phone', document.getElementById('phone').value);
-    validarFormulario();
-    }
+        setear()
+    }  
 }
 
-function validarFormulario() { 
-    let firstNameVal = document.getElementById('firstName').value;
-     if(firstNameVal.length == 0) {
-      return; 
-    } 
-    let firstLastNameVal = document.getElementById('firstLastName').value;
-     if (firstLastNameVal.length == 0) { 
-     return; 
-    } 
-    let emailVal = document.getElementById('email').value;
-     if (emailVal.length == 0) { 
-     return; 
-    } 
-    let phoneVal = document.getElementById('phone').value;
-     if (phoneVal.length == 0) { 
-     return; 
-    }   else swal("¡Sus cambios se han guardado con éxito! :)", {
-        icon: "success",
-        }
-      )
-      .then(function() {
-        window.location.href = "my-profile.html";
-        
-      });    
- }
  function formReady(){
-    let firstName = localStorage.getItem("firstName");
-    let secondName = localStorage.getItem("secondName");
-    let firstLastName = localStorage.getItem("firstLastName");
-    let secondLastName = localStorage.getItem("secondLastName");
-    let email = localStorage.getItem("email");
-    let phone = localStorage.getItem("phone");
-    if (firstName &&  firstLastName && email && phone) {
+    let retrievedObject = JSON.parse(localStorage.getItem('datosJSON'));
+    let firstName = retrievedObject.firstName;
+    let secondName = retrievedObject.secondName;
+    let firstLastName = retrievedObject.firstLastName;
+    let secondLastName = retrievedObject.secondLastName;
+    let email = retrievedObject.email;
+    let phone = retrievedObject.phone;
+    if (firstName &&  firstLastName && email && phone) {    
             document.getElementById('divMaster').innerHTML = `
+            <div >
                     <div class="row justify-content-md-center">
                      <div class="col-md-8 order-md-1">
                              <form>
@@ -56,27 +38,27 @@ function validarFormulario() {
                                     <div class="row">
                                     <div class="col-md-6 mb-3" style="display: inline-block">
                                     <label for="firstName" style="display: block;"><b>Primer nombre</b></label>
-                                    <p> `+ firstName+`</p>
+                                    <p class="pstyle"> `+ firstName+`</p>
                                     </div>
                                     <div class="col-md-6 mb-3"> 
                                     <label for="secondName" style="display: block;" id="segundo"><b>Segundo nombre</b></label>
-                                    <p> `+ secondName+`</p>
+                                    <p class="pstyle" id="secondName"> `+ secondName+`</p>
                                     </div>
                                     <div class="col-md-6 mb-3" style="display: inline-block">
                                     <label for="firstLastName" style="display: block;"><b>Primer apellido</b></label>
-                                    <p> `+ firstLastName+`</p>
+                                    <p class="pstyle"> `+ firstLastName+`</p>
                                     </div>
                                     <div class="col-md-6 mb-3">
                                     <label for="secondLastName" style="display: block;" id="segundoApellido"><b>Segundo apellido</b></label>
-                                    <p> `+ secondLastName+`</p>
+                                    <p class="pstyle" id="secondLastName"> `+ secondLastName+`</p>
                                     </div>
                                     <div class="col-md-6 mb-3">
                                     <label for="email"><b>Email</b></label>
-                                    <p> `+ email+`</p>
+                                    <p class="pstyle"> `+ email+`</p>
                                     </div>
                                     <div class="col-md-6 mb-3">
                                     <label for="phone"><b>Teléfono de contacto</b></label>
-                                    <p> `+ phone+`</p>
+                                    <p class="pstyle"> `+ phone+`</p>
                                     </div>
                                     </div>
                                     <hr class="mb-4">
@@ -85,52 +67,32 @@ function validarFormulario() {
                             </div>
                         </div>
                     </div>
+                    </div>
                         `
                         if(!secondName) {
-                            document.getElementById("segundo").innerHTML = ""
+                            document.getElementById("segundo").innerHTML = "";
+                            let element = document.getElementById("secondName");
+                            element.classList.remove("pstyle");
                         }
                         if(!secondLastName) {
-                            document.getElementById("segundoApellido").innerHTML = ""
+                            document.getElementById("segundoApellido").innerHTML = "";
+                            let elements = document.getElementById("secondLastName");
+                            elements.classList.remove("pstyle");
                         } 
-    }
-    
- }
-            formReady()
-            document.getElementById("modificar").onclick = function(e) { 
-                localStorage.removeItem("firstName")
-                localStorage.removeItem("secondName")
-                localStorage.removeItem("firstLastName")
-                localStorage.removeItem("secondLastName")
-                localStorage.removeItem("email")
-                localStorage.removeItem("phone")
-                window.location.href = "my-profile.html"
-            }
-//  showValues();
-//  function showValues() {
-//     let firstName = localStorage.getItem("firstName");
-//     let secondName = localStorage.getItem("secondName");
-//     let firstLastName = localStorage.getItem("firstLastName");
-//     let secondLastName = localStorage.getItem("secondLastName");
-//     let email = localStorage.getItem("email");
-//     let phone = localStorage.getItem("phone");
-//     if (firstName) {
-//     document.getElementById("firstName").value = firstName;
-//     }
-//     if (secondName) {
-//     document.getElementById("secondName").value = secondName;
-//     }
-//     if (firstLastName) {
-//     document.getElementById("firstLastName").value = firstLastName;
-//     }
-//     if (secondLastName) {
-//     document.getElementById("secondLastName").value = secondLastName;
-//     }
-//     if (email) {
-//     document.getElementById("email").value = email;
-//     }
-//     if (phone) {
-//         document.getElementById("phone").value = phone;
-//         }
-//  }
 
+                        
+    }
+    document.getElementById("modificar").onclick = function(e) { 
+        localStorage.removeItem("datosJSON")
+        window.location.href = "my-profile.html"
+    }
+ }
+
+ function formReadyShow(){
+    if(localStorage.getItem('datosJSON')) {
+        formReady()
+    }
+}
+formReadyShow()
+            
 });
